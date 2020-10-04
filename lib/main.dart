058b0291f715a76e9deff852745e199c980bc9be
Clarisse Fourie <clarisse.fourie@gmail.com>
2020-10-04 14:45:20 -0600

    Added row and first 3 containers

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,57 @@ class MyApp extends StatelessWidget {
           title: Text('Row and Column Widgets'),
           backgroundColor: Colors.blue,
         ),
-        body: Center(
-          child: Text(
-            'Hey',
-          ),
+        body: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                  color: Colors.amber,
+                  border: Border.all(color: Colors.black, width: 3)),
+              child: Center(
+                child: Text(
+                  'Container 1',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+              ),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  'Container 3',
+                ),
+              ),
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.black,
+                border: Border.all(
+                  color: Colors.white,
+                  width: 3,
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  'Container 5',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
