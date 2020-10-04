ee8f689f782c025468a2a7fba71fe7b2342311a8
Clarisse Fourie <clarisse.fourie@gmail.com>
2020-10-04 16:32:36 -0600

    Final Commit - Adjusted 3rd column

--- lib/main.dart
+++ lib/main.dart
@@ -94,38 +94,44 @@ class MyApp extends StatelessWidget {
             ),
             Column(
               children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.black,
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 3,
-                    ),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Container 5',
-                      style: TextStyle(
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.black,
+                      border: Border.all(
                         color: Colors.white,
+                        width: 3,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Container 5',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
                 ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                  ),
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      'Con 6',
-                      style: TextStyle(
-                        fontSize: 30,
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                    ),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Con 6',
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
                       ),
                     ),
                   ),
@@ -138,52 +144,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-/*children: <Widget>[
-            Container(
-              height: 100,
-              width: 100,
-              decoration: BoxDecoration(
-                  color: Colors.amber,
-                  border: Border.all(color: Colors.black, width: 3)),
-              child: Center(
-                child: Text(
-                  'Container 1',
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              decoration: BoxDecoration(
-                color: Colors.yellow,
-              ),
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Text(
-                  'Container 3',
-                ),
-              ),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.black,
-                border: Border.all(
-                  color: Colors.white,
-                  width: 3,
-                ),
-              ),
-              child: Center(
-                child: Text(
-                  'Container 5',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            )
-          ], */
